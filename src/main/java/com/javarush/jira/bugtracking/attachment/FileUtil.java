589751616a8f74bb9e4e43b4c50ad02a020b3db4
Dmitriy Kim <dmitriy,kim@outlook.com>
2024-03-13 15:55:03 +0200

    Сделать рефакторинг метода com.javarush.jira.bugtracking.attachment.FileUtil#upload чтоб он использовал современный подход для работы с файловой системмой.ndex

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,32 +7,47 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 @UtilityClass
 public class FileUtil {
     private static final String ATTACHMENT_PATH = "./attachments/%s/";
+    private static final int BUFFER_SIZE = 256;
 
     public static void upload(MultipartFile multipartFile, String directoryPath, String fileName) {
         if (multipartFile.isEmpty()) {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
+        Path path = Paths.get(directoryPath);
+        if (!Files.exists(path)) {
+            try {
+                Files.createDirectory(path);
             }
+            catch (IOException e) {
+                throw new IllegalRequestDataException("Failed to create directory " + path.getFileName());
+            }
+        }
+        Path filePath = Paths.get(directoryPath + fileName);
+        try(FileChannel ch = (FileChannel) Files.newByteChannel(filePath, StandardOpenOption.WRITE)) {
+            ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+            byte[] bytes = multipartFile.getBytes();
+            for (byte b:bytes) {
+                for(int i = 0; i < BUFFER_SIZE; i++)
+                    buffer.put(b);
+                buffer.rewind();
+                ch.write(buffer);
+            }
+        }
+        catch (IOException e){
+            throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
         }
     }
 
